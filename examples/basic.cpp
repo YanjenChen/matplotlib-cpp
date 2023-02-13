e2e6b44c442cda896a7f575e1715f5769aa010a5
yjchen <yanjen.chen4453@gmail.com>
2023-02-13 17:12:45 +0800

    update basic example

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -17,7 +17,7 @@ int main()
     }
     
     // Set the size of output image = 1200x780 pixels
-    plt::figure_size(1200, 780);
+    // plt::figure_size(1200, 780);
 
     // Plot line from given x and y data. Color is selected automatically.
     plt::plot(x, y);
